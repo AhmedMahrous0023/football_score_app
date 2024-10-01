2984513f1c0668571f65be393a9f15010701b331
Ahmed Mahrous <medhighdiamond2023@gmail.com>
2024-10-01 20:42:29 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/football_score_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.football_score_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const ksecondryColor = Color(0xff1b1b1b);
+const kprimaryColor = Color(0xfff2f3f4);
+const blueTextColor = Color(0xff035997);
+const detailBackgroundColor = Color.fromARGB(255, 117, 143, 162);
+
